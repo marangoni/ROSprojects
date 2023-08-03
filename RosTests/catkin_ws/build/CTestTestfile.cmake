c92c9b095d61e64f043a299536440dc75af2866b
marangoni <oliveiralcm@gmail.com>
2023-08-02 23:48:16 -0300

    * Configuração do arquivo urdf para o rviz e gazebo

--- RosTests/catkin_ws/build/CTestTestfile.cmake
+++ RosTests/catkin_ws/build/CTestTestfile.cmake
@@ -5,9 +5,15 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("gazebo_ros_pkgs/gazebo_dev")
+subdirs("gazebo_ros_pkgs/gazebo_ros_pkgs")
 subdirs("arduino_control_tutorials")
 subdirs("jkprobot")
 subdirs("robogui")
-subdirs("robot")
 subdirs("tangs")
+subdirs("gazebo_ros_pkgs/gazebo_msgs")
+subdirs("gazebo_ros_pkgs/gazebo_ros")
+subdirs("gazebo_ros_pkgs/gazebo_plugins")
+subdirs("gazebo_ros_pkgs/gazebo_ros_control")
+subdirs("tangs5dof")
 subdirs("urdf_tutorial")
--- RosTests/catkin_ws/build/robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luiz/ROSprojects/RosTests/catkin_ws/src/robot
-# Build directory: /home/luiz/ROSprojects/RosTests/catkin_ws/build/robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
