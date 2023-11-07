e427ea475e95accb1652ddb68681d9c04ad54538
marangoni <oliveiralcm@gmail.com>
2023-11-07 18:16:26 -0300

    Versao simplificada dos pacotes - versao final IC

--- RosTests/catkin_ws/build/urdf_tutorial/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luiz/ROSprojects/RosTests/catkin_ws/src/urdf_tutorial
-# Build directory: /home/luiz/ROSprojects/RosTests/catkin_ws/build/urdf_tutorial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_urdf_tutorial_roslaunch-check_launch "/home/luiz/ROSprojects/RosTests/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python3" "/opt/ros/noetic/share/catkin/cmake/test/run_tests.py" "/home/luiz/ROSprojects/RosTests/catkin_ws/build/test_results/urdf_tutorial/roslaunch-check_launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/luiz/ROSprojects/RosTests/catkin_ws/build/test_results/urdf_tutorial" "/opt/ros/noetic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/luiz/ROSprojects/RosTests/catkin_ws/build/test_results/urdf_tutorial/roslaunch-check_launch.xml\" \"/home/luiz/ROSprojects/RosTests/catkin_ws/src/urdf_tutorial/launch\" ")
-set_tests_properties(_ctest_urdf_tutorial_roslaunch-check_launch PROPERTIES  _BACKTRACE_TRIPLES "/opt/ros/noetic/share/catkin/cmake/test/tests.cmake;160;add_test;/opt/ros/noetic/share/roslaunch/cmake/roslaunch-extras.cmake;66;catkin_run_tests_target;/home/luiz/ROSprojects/RosTests/catkin_ws/src/urdf_tutorial/CMakeLists.txt;9;roslaunch_add_file_check;/home/luiz/ROSprojects/RosTests/catkin_ws/src/urdf_tutorial/CMakeLists.txt;0;")
